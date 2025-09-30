b98bc5d40244e36d26cca7b710f68d49b377ebc7
Uchantal <u.chantal@alustudent.com>
2025-09-30 11:14:53 +0200

    installed dependencies and added serializer

--- safeboda/settings.py
+++ safeboda/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "users",
     "rest_framework",
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'drf_spectacular',
 
 ]
 
@@ -141,6 +144,31 @@ CACHES = {
     }
 }
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_AUTHENTICATION_CLASSES': []
-# }
+
+# REST Framework Configuration
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+# JWT Settings
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ROTATE_REFRESH_TOKENS': True,
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
+
+# API Documentation Settings
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'SafeBoda API',
+    'DESCRIPTION': 'API for SafeBoda ride-sharing platform',
+    'VERSION': '1.0.0',
+}
--- users/serializers.py
+++ users/serializers.py
@@ -1,36 +1,44 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
 
-from users.models import User, Passenger, Rider
+User = get_user_model()
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    """Serializer for user registration"""
+    password = serializers.CharField(
+        write_only=True, 
+        required=True, 
+        validators=[validate_password]
+    )
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('email', 'password', 'password2', 'first_name', 
+                  'last_name', 'phone_number', 'user_type')
+        extra_kwargs = {
+            'first_name': {'required': True},
+            'last_name': {'required': True},
+        }
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError(
+                {"password": "Password fields didn't match."}
+            )
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = User.objects.create_user(**validated_data)
+        return user
 
 
 class UserSerializer(serializers.ModelSerializer):
+    """Serializer for user profile"""
     class Meta:
         model = User
-        fields = ['id', 'email', 'first_name', 'last_name', 'phone_number', 'user_type']
-
-
-class PassengerSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
-    
-    class Meta:
-        model = Passenger
-        fields = [
-            'id', 'user', 'passenger_id', 'preferred_payment_method', 
-            'home_address', 'profile_picture', 'preferred_language', 
-            'emergency_contact', 'is_verified', 'created_at', 'updated_at'
-        ]
-        read_only_fields = ['id', 'created_at', 'updated_at']
-
-
-class RiderSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
-    
-    class Meta:
-        model = Rider
-        fields = [
-            'id', 'user', 'profile_picture', 'license_number', 'license_picture',
-            'id_number_picture', 'verification_status', 'verification_notes',
-            'is_available', 'current_latitude', 'current_longitude', 
-            'average_rating', 'total_rides', 'total_earnings', 'created_at', 'updated_at'
-        ]
-        read_only_fields = ['id', 'average_rating', 'total_rides', 'total_earnings', 'created_at', 'updated_at']
+        fields = ('id', 'email', 'first_name', 'last_name', 
+                  'phone_number', 'user_type', 'date_joined')
+        read_only_fields = ('id', 'date_joined')
